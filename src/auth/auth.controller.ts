520659910548680a2dd260cfc29ec319c82c42cd
NguyenVanThuan <thuannguyenvan@tcom.vn>
2023-10-11 01:11:06 +0700

    add login api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { RegisterUserDTO } from './dto/register-user.dto';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/entities/user.entity';
+import { LoginUserDTO } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -13,4 +14,9 @@ export class AuthController {
         return this.authService.register(registerUserDTO);
     }
 
+    @Post('login')
+    @UsePipes(ValidationPipe)
+    login(@Body() loginUserDTO:LoginUserDTO):Promise<any> {
+        return this.authService.login(loginUserDTO);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { RegisterUserDTO } from './dto/register-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDTO } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
     constructor(
-        @InjectRepository(User) private userRepository:Repository<User>
+        @InjectRepository(User) private userRepository:Repository<User>,
+        private jwtService:JwtService
     ){}
 
     async register(registerUserDTO:RegisterUserDTO):Promise<User> {
@@ -16,10 +19,42 @@ export class AuthService {
         return await this.userRepository.save({...registerUserDTO,refreshToken:"refresh_token_string",password:hashPassWord});
     }
 
+    async login(loginUserDTO:LoginUserDTO):Promise<any> {
+        const user = await this.userRepository.findOne(
+            {
+                where:{email:loginUserDTO.email}
+            }
+        )
+        if(!user) {
+            throw new HttpException("Email is not exist",HttpStatus.UNAUTHORIZED);
+        }
+        const checkPass = bcrypt.compareSync(loginUserDTO.password,user.password);
+
+        if(!checkPass) {
+            throw new HttpException('Password is not correct',HttpStatus.UNAUTHORIZED);
+        }
+        // generate access token and refresh token
+         const payload = {id:user.id, email:user.email};
+         return this.generateToken(payload);
+    }
+
     private async hashPassWord(password: string):Promise<string> {
         const saltRound = 10;
         const salt = await bcrypt.genSalt(saltRound);
         const hash = await bcrypt.hash(password,salt);
         return hash;
+    }
+
+    private async generateToken(payload: {id:number,email:string}) {
+        const accessToken = await this.jwtService.signAsync(payload);
+        const refreshToken = await this.jwtService.signAsync(payload,{
+            secret:'123456',
+            expiresIn:'1h'
+        });
+        await this.userRepository.update(
+            {email: payload.email},
+            {refreshToken:refreshToken}
+        )
+        return {accessToken,refreshToken};
     }
 }
